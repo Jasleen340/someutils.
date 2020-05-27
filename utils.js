f854247057f4e3c401898fd77ebbf1afc77a42fe
Bhagwant Singh <bhagwantrandhawa5@gmail.com>
2020-05-27 09:45:50 -0700

    added function add

--- utils.js
+++ utils.js
@@ -6,21 +6,7 @@
  }
  console.log(pow(x,y));
 
-//  let x=parseInt(process.argv[2])
-//  let y=parseInt(process.argv[3])
-//  function div (x,y){
-//  let division = x/y
-//  return division
-//  console.log(div(x,y));
-//  }
 
-// let x=parseInt(process.argv[2])
-// let y=parseInt(process.argv[3])
-// function pow (x,y){
-// let power = x**y
-// return power
-// }
-// console.log(pow(x,y));
 
 let x=parseInt(process.argv[2])
 let y=parseInt(process.argv[3])
@@ -29,3 +15,11 @@ let subtract = x-y
 return subtract
 }
 console.log(sub(x,y));
+
+function add(a,b) {
+ 
+    return a+b
+
+}
+console.log(add(a,b));
+
