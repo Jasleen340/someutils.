d517543d81bbb336208293521096d47ef29857e0
Jasleen Singh <jasleensingh340@gmail.com>
2020-05-27 14:03:37 -0700

    code for product of two numbers

--- utils.js
+++ utils.js
@@ -1,18 +1,18 @@
-// let x=parseInt(process.argv[2])
-// let y=parseInt(process.argv[3])
-// function pow (x,y){
-// let power = x*y
-// return power
-// }
-// console.log(pow(x,y));
-
  let x=parseInt(process.argv[2])
  let y=parseInt(process.argv[3])
- function div (x,y){
- let division = x/y
- return division
- console.log(div(x,y));
+ function pow (x,y){
+ let power = x*y
+ return power
  }
+ console.log(pow(x,y));
+
+//  let x=parseInt(process.argv[2])
+//  let y=parseInt(process.argv[3])
+//  function div (x,y){
+//  let division = x/y
+//  return division
+//  console.log(div(x,y));
+//  }
 
 // let x=parseInt(process.argv[2])
 // let y=parseInt(process.argv[3])
