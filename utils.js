e87fa4bbd072203387001a7fe93726d84916e0c7
Jasleen Singh <jasleensingh340@gmail.com>
2020-05-27 14:02:02 -0700

    add code for division

--- utils.js
+++ utils.js
@@ -6,19 +6,19 @@
 // }
 // console.log(pow(x,y));
 
+ let x=parseInt(process.argv[2])
+ let y=parseInt(process.argv[3])
+ function div (x,y){
+ let division = x/y
+ return division
+ console.log(div(x,y));
+ }
+
 // let x=parseInt(process.argv[2])
 // let y=parseInt(process.argv[3])
-// function div (x,y){
-// let division = x/y
-// return division
+// function pow (x,y){
+// let power = x**y
+// return power
 // }
-// console.log(div(x,y));
-
-let x=parseInt(process.argv[2])
-let y=parseInt(process.argv[3])
-function pow (x,y){
-let power = x**y
-return power
-}
-console.log(pow(x,y));
+// console.log(pow(x,y));
 
