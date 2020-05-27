c1506a6d02244ceb7df4a6d1a184890b12364c2a
Jasleen Singh <jasleensingh340@gmail.com>
2020-05-27 14:09:09 -0700

    code for subtraction

--- utils.js
+++ utils.js
@@ -22,3 +22,10 @@
 // }
 // console.log(pow(x,y));
 
+let x=parseInt(process.argv[2])
+let y=parseInt(process.argv[3])
+function sub (x,y){
+let subtract = x-y
+return subtract
+}
+console.log(sub(x,y));
